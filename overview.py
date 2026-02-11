0d0255b1cfbdd3c8bb05738f115662254383490c
kodumurisandeep14 <kodumurisandeep14@gmail.com>
2026-02-11 11:28:39 -0500

    python completed

---
+++ overview.py
@@ -0,0 +1,96 @@
+# str="Python!"
+# print(str)
+
+# age=50
+# print(age)
+
+# price=50.5
+# print(price)
+
+# is_student=True
+# print(is_student)
+
+# x="10"
+# y=int(x)
+# print(y*y)# type casting
+
+# num1=10
+# num2=3
+
+# print(num1>num2)
+# print(num1>5 and num2<5)
+
+# age=16
+# if age>=18:
+#     print("Major")
+# else:
+#     print("Minor")
+
+# marks=85
+# if marks>=90:
+#     print("A grade")
+# elif marks>=75:
+#     print("B grade")
+# else:
+#     print("C grade")
+
+# def test_fun():
+#     print("No Parameter - no return  type")
+# test_fun()
+
+# def test_fun():
+#     return "No Parameter - with return  type"
+# res = test_fun()
+# print(res)
+
+# def test_fun(param1):
+#     print (param1)
+# test_fun("with parameter and no return type")
+
+# def test_fun(param1):
+#     return param1
+
+# # res=test_fun("with parameter and no return type")
+# # print(res)
+
+# score={
+#     90:" A grade",
+#     80: "B grade",
+#     70: "C grade",
+#     60:"Fail"
+# }
+# print(score)
+# print(score.get(90))
+# print(score.get(40,"Invalid"))
+
+# mathematics ={
+#     "+" : lambda num1,num2 :num1+num2,
+#     "-" : lambda num1,num2 :num1-num2,
+#     "*" : lambda num1,num2 :num1*num2,
+#     "/" : lambda num1,num2 :num1/num2,
+# }
+
+# print(mathematics["+"](200,300))
+
+# numbers=[10,20,30,40,50]
+# for num in numbers:
+#     print(num)
+
+# tuple=(100,200,300)
+# print(tuple)
+# print(tuple[0])
+# print(tuple[-1])
+
+# for t in tuple:
+#     print(t)
+
+a, *b =(1,2,3,4,5)
+print(a)
+for num in b:
+    print(b)
+
+
+
+
+
+
